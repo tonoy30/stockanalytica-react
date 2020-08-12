324731994c2c89871ea99427c965e3eba47f5122
diptomondal007 <diptomondal007@gmail.com>
2020-08-12 13:56:23 +0600

    k8s deployment added

---
+++ build/precache-manifest.c4922d42851df6e575439a206ea2e7f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fbffa5f986a13a4658c342b5127011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5efa5c4e5eb7da590933",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35270ed1fae7795ecd11",
+    "url": "/static/js/2.09e223b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09e223b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5efa5c4e5eb7da590933",
+    "url": "/static/js/main.04da5951.chunk.js"
+  },
+  {
+    "revision": "64319b1dd7867ce13826",
+    "url": "/static/js/runtime-main.79f48447.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
