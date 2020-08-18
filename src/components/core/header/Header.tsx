74542fc8c33487dd72bec91378a06c8de7893c5d
tonoy <tonoy.sust@gmail.com>
2020-08-18 16:28:13 +0600

    style

--- src/components/core/header/Header.tsx
+++ src/components/core/header/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import "./Header.scss"
 const Header = () => {
 	return (
-		<div>
-			Header
+		<div className="header">
+			<p>
+				Header
+			</p>
 			<ul>
 				<li>
 					<Link to="/" replace>Home</Link>
