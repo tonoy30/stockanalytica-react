f3a52b063befd26761c81e1f95e8c4c53095fcba
Md Tonoy Akando <tonoy.sust@gmail.com>
2020-10-05 23:55:26 +0600

    idk

---
+++ build/precache-manifest.3e82b292d638d827ebed8f28349a45b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459e80ae12da2c573eb0e58810196b4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b61a4b83b3f2c28c988",
+    "url": "/static/css/main.7892c085.chunk.css"
+  },
+  {
+    "revision": "303dfe70b537a9bfead7",
+    "url": "/static/js/2.ff0dedd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ff0dedd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b61a4b83b3f2c28c988",
+    "url": "/static/js/main.4fe1495a.chunk.js"
+  },
+  {
+    "revision": "64319b1dd7867ce13826",
+    "url": "/static/js/runtime-main.79f48447.js"
+  }
+]);
--- src/components/core/header/Header.tsx
+++ src/components/core/header/Header.tsx
@@ -1,43 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaReact } from "react-icons/fa"
-import "./Header.scss"
+import { Box, Flex, Heading, Text } from "@chakra-ui/core";
+import React, { useState } from "react";
+import "./Header.scss";
 //TODO: will be in the separate file
 const list = [
-	{
-		"title": "Home",
-		"url": "/home"
-	},
-	{
-		"title": "Services",
-		"url": "/services"
-	},
-	{
-		"title": "About",
-		"url": "/about"
-	},
-	{
-		"title": "Contact Us",
-		"url": "/contact_us"
-	}
-]
-const Header = () => {
-	return (
-		<div className="header">
-			<h1 className="logo">React
-				<span>
-					<FaReact />
-				</span>
-			</h1>
-			<ul className="menu">
-				{list.map((item) => (
-					<li key={item.title}>
-						<Link to={item.url}>{item.title}</Link>
-					</li>
-				))}
-			</ul>
-		</div>
-	)
-}
+  {
+    title: "Home",
+    url: "/home",
+  },
+  {
+    title: "Services",
+    url: "/services",
+  },
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Contact Us",
+    url: "/contact_us",
+  },
+];
+const MenuItem = (props: any) => (
+  <Text mt={{ base: 4, md: 0 }} mr={6} display="block">
+    {props.children}
+  </Text>
+);
+const Header = (props: any) => {
+  const [show, setShow] = useState(false);
+  const handleToggle = () => setShow(!show);
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="teal.500"
+      color="white"
+      background="radial-gradient(circle, #5c27fb 0%, #7047f7 100%)"
+      {...props}
+    >
+      <Flex align="center" mr={10}>
+        <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+          Stock Analytica
+        </Heading>
+      </Flex>
+      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+        <svg
+          fill="white"
+          width="12px"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </Box>
+      <Box
+        display={{ sm: show ? "block" : "none", md: "flex" }}
+        width={{ sm: "full", md: "auto" }}
+        alignItems="center"
+        flexGrow={1}
+      >
+        <MenuItem>Docs</MenuItem>
+        <MenuItem>Examples</MenuItem>
+        <MenuItem>Blog</MenuItem>
+      </Box>
+    </Flex>
+  );
+};
 
-export default Header
+export default Header;
