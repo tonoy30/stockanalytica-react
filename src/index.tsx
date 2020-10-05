e48352d2ab93d29f27f68d4a0464adab1abc4cbc
Md Tonoy Akando <tonoy.sust@gmail.com>
2020-10-06 00:48:57 +0600

    path resolutation

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,23 @@
-import { CSSReset, ThemeProvider } from "@chakra-ui/core";
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import "./index.scss";
-import * as serviceWorker from "./serviceWorker";
-import { getStore } from "./store";
-import { newTheme } from "./themes/breakpoints";
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { Navigator } from '@navigator/Navigator';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './index.scss';
+import * as serviceWorker from './serviceWorker';
+import { getStore } from './store';
+import { newTheme } from './themes/breakpoints';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={getStore()}>
-      <ThemeProvider theme={newTheme}>
-        <CSSReset />
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Provider store={getStore()}>
+			<ThemeProvider theme={newTheme}>
+				<CSSReset />
+				<Navigator />
+			</ThemeProvider>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,21 @@
+import Layout from '@components/core/layout/Layout';
+import Home from '@components/home/Home';
+import Test from '@components/test/Test';
+import React, { FC } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+type Props = {};
+
+const Navigator: FC<Props> = () => {
+	return (
+		<Router>
+			<Layout>
+				<Switch>
+					<Route exact={true} path='/home' component={Home} />
+					<Route path='/test' component={Test} />
+				</Switch>
+			</Layout>
+		</Router>
+	);
+};
+export { Navigator };
